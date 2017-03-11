ebefef797dc29f06ed93bbd6d746d94d39f33893
bloonz <margo.kaljuvee@gmail.com>
2017-03-11 17:31:45 +0100

    drinking buddies

---
+++ aip_mini/Assets/Scripts/AI/Actions/DrinkAtBar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Apex.AI;
+
+public class DrinkAtBar : ActionBase
+{
+
+    public override void Execute(IAIContext context)
+    {
+        var c = (Context)context;
+
+        var player = c.self;
+
+        player.GetComponent<Player>().SetActionText("Drinking with buddies");
+
+        //check where the player is
+        //if not at bar, then go to bar
+        if (!player.GetComponent<Player>().IsAtBar())
+        {
+            player.GetComponent<Player>().GoToBar();
+        }
+
+        player.GetComponent<Player>().SetLastPartyTime(c.time);
+
+        int money_amount = player.GetComponent<Player>().GetMoneyAmount() - 1;
+
+        player.GetComponent<Player>().SetMoneyAmount(money_amount);
+    }
+}
---
+++ aip_mini/Assets/Scripts/AI/Scorers/DrinkingBuddies.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Apex.AI;
+
+public class DrinkingBuddies : ContextualScorerBase
+{
+
+    public override float Score(IAIContext context)
+    {
+        var c = (Context)context;
+        var player = c.self;
+
+        int buddies = c.people_at_bar;
+
+        return c.people_at_bar > this.score ? this.score : 0f;
+    }
+}
