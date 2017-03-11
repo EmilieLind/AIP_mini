bd1f99861458442f5c201cc81ec6e6f1426a212a
bloonz <margo.kaljuvee@gmail.com>
2017-03-11 17:29:55 +0100

    materials

---
+++ aip_mini/Assets/Scripts/AI/Scorers/PartyItch.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Apex.AI;
+
+public class PartyItch : ContextualScorerBase
+{
+
+    public override float Score(IAIContext context)
+    {
+        var c = (Context)context;
+        var player = c.self;
+
+        float time_since_last_party = c.time - player.GetComponent<Player>().GetLastPartyTime();
+
+        float party_urge = Mathf.Max(0f, time_since_last_party - this.score);
+
+        player.GetComponent<Player>().SetPartyUrge(party_urge.ToString());
+
+        return party_urge;
+    }
+}
